840a3e565dc2168f3402c17ce013afb55ec16667
Prakhar Gupta <prakhargupta3680@gmail.com>
2025-03-12 11:46:00 +0530

    Made Multiple Asteroids

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,48 @@ import { Canvas } from "@react-three/fiber";
 import Asteroid from "./components/Asteroid";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Physics } from "@react-three/rapier";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [asteroidPosition, setAsteroidPosition] = useState([
+    { id: 1, position: [90, 90, 90] },
+  ]);
+
+  function randomValue() {
+    const value = Math.random() * 40 + 50; // Generates a number between 50 and 90
+    return Math.random() < 0.5 ? -value : value; // Randomly makes it negative
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAsteroidPosition((prev) => [
+        ...prev,
+        {
+          id: prev.length + 1,
+          position: [randomValue(), randomValue(), randomValue()],
+        },
+      ]);
+    }, 2000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, [asteroidPosition]);
+
   return (
     <Canvas camera={{ position: [0, 0, 10] }}>
-      <Stars fade={true} count={1000} />
+      <Stars fade={true} count={500} />
 
       <Physics gravity={[0, 0, 0]}>
-        <Asteroid position={[90, 90, 90]} />
+        {asteroidPosition.map((e) => {
+          return (
+            <Asteroid
+              key={e.id}
+              position={e.position as [number, number, number]}
+            />
+          );
+        })}
       </Physics>
 
+      
       <ambientLight />
       <OrbitControls />
     </Canvas>
