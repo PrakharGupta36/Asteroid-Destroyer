f243e893f0066371a4b5c25db4937d14f2324ce3
Prakhar Gupta <prakhargupta3680@gmail.com>
2025-03-14 16:57:52 +0530

    Forget to persist the settings in local-storage

--- src/hooks/State.tsx
+++ src/hooks/State.tsx
@@ -1,9 +1,9 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface useGameTypes {
   start: boolean;
   setStart: (value: boolean) => void;
-
   settings: {
     id: number;
     text: string;
@@ -12,57 +12,61 @@ interface useGameTypes {
   }[];
 }
 
-const useGame = create<useGameTypes>((set) => ({
-  start: false,
-  setStart: (value: boolean) => set(() => ({ start: value })),
-
-  settings: [
+const useGame = create<useGameTypes>()(
+  persist(
+    (set) => ({
+      start: false,
+      setStart: (value: boolean) => set(() => ({ start: value })),
+      settings: [
+        {
+          id: 1,
+          text: "Music",
+          value: false,
+          action: () =>
+            set((state) => {
+              const settings = state.settings.map((e) =>
+                e.id === 1 ? { ...e, value: !e.value } : e
+              );
+              return {
+                settings,
+              };
+            }),
+        },
+        {
+          id: 2,
+          text: "Sounds",
+          value: true,
+          action: () =>
+            set((state) => {
+              const settings = state.settings.map((e) =>
+                e.id === 2 ? { ...e, value: !e.value } : e
+              );
+              return {
+                settings,
+              };
+            }),
+        },
+        {
+          id: 3,
+          text: "Post Processing",
+          value: true,
+          action: () =>
+            set((state) => {
+              const settings = state.settings.map((e) =>
+                e.id === 3 ? { ...e, value: !e.value } : e
+              );
+              return {
+                settings,
+              };
+            }),
+        },
+      ],
+    }),
     {
-      id: 1,
-      text: "Music",
-      value: false,
-      action: () =>
-        set((state) => {
-          const settings = state.settings.map((e) =>
-            e.id === 1 ? { ...e, value: !e.value } : e
-          );
-
-          return {
-            settings,
-          };
-        }),
-    },
-    {
-      id: 2,
-      text: "Sounds",
-      value: true,
-      action: () =>
-        set((state) => {
-          const settings = state.settings.map((e) =>
-            e.id === 2 ? { ...e, value: !e.value } : e
-          );
-
-          return {
-            settings,
-          };
-        }),
-    },
-    {
-      id: 3,
-      text: "Post Processing",
-      value: true,
-      action: () =>
-        set((state) => {
-          const settings = state.settings.map((e) =>
-            e.id === 3 ? { ...e, value: !e.value } : e
-          );
-
-          return {
-            settings,
-          };
-        }),
-    },
-  ],
-}));
+      name: "game-settings", // name of the item in localStorage
+      partialize: (state) => ({ settings: state.settings }), // only store the settings array
+    }
+  )
+);
 
 export default useGame;
